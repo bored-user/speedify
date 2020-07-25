5291893b0289274904ef2fb20489dae5138afc4d
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-07-25 14:17:21 -0300

    Add new comments and fix opt remove bug

--- speedify.js
+++ speedify.js
@@ -9,7 +9,7 @@
     while (!$(settingsBtn).length)
         await new Promise(res => setTimeout(res, 500));
 
-    // YouTube creates the menu dynamically.
+    // YouTube creates the menu dynamically when the user clicks on the Settings icon. Setting EL for that.
     $(settingsBtn).on('click', function () {
         $(speedOption).on('click', async function () {
             settingsBtn = $(settingsBtn);
@@ -20,6 +20,11 @@
             await new Promise(res => setTimeout(res, 1000));
 
             speedRates.forEach(speedRate => {
+                // If that speed label was already added, return.
+                // YT "randomly" seems to remove the speed labels and rebuild. So, I cannot just remove the click EL;
+                // I have to rebuild all new labels everytime.
+                if ($(`.ytp-menuitem:contains('${speedRate}')`, speedRatesMenu).length) return;
+
                 // Creating speed labels
                 const speedBtn = document.createElement('div');
                 speedBtn.classList.add('ytp-menuitem');
@@ -38,8 +43,6 @@
                 // Appending to menu
                 $(speedRatesMenu).last().append(speedBtn);
             });
-
-            $(this).off('click');
         });
 
         $(this).off('click');
